59a83f0289d9d0b44f470ac71af86721ea8be7b4
SHARVESWAR MADASAMY <msharveswar220@gmail.com>
2026-03-15 16:53:35 +0530

    Fix: Update version to 0.1.1 in __init__.py

--- ssblast/__init__.py
+++ ssblast/__init__.py
@@ -3,7 +3,7 @@
 
 from .solver import solve, CUPY_AVAILABLE, TRITON_AVAILABLE
 
-__version__ = "0.1.0"
-__author__  = "Sharvesh"
+__version__ = "0.1.1"
+__author__  = "SHARVESWAR MADASAMY"
 
 __all__ = ["solve"]
